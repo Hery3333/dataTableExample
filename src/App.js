553add8639e78826fed552621c1be7829232bf35
hery3333 <heryn3333@gmail.com>
2022-06-04 21:38:13 +0200

    add filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {GridComponent} from '@syncfusion/ej2-react-grids'
+import {GridComponent, ColumnsDirective, ColumnDirective,Page,Inject,Filter} from '@syncfusion/ej2-react-grids'
 import data from './dataSource.json';
 
 import './App.css';
@@ -6,7 +6,21 @@ import './App.css';
 function App() {
   return (
     <div style={{ margin: '10%', marginTop: '5%'}}>
-      <GridComponent dataSource={data}/>
+      <GridComponent 
+        dataSource={data} 
+        allowPaging={true}
+        pageSettings={{ pageSize:5}}
+        allowFiltering={true}
+        >
+        <ColumnsDirective>
+          <ColumnDirective field="OrderID" headerText='Invoice ID' textAlign='center' width='100'/>
+          <ColumnDirective field='CustomerID' headerText='Customer ID' width='150'/>
+          <ColumnDirective field='ShipCountry' headerText='Ship Country'/>
+          <ColumnDirective field='ShipName' headerText='Ship Name'/>
+          <ColumnDirective field='Freight' textAlign='Right' format='C2' width='100'/>
+        </ColumnsDirective>
+        <Inject services={[Page, Filter]}/>
+      </GridComponent>
     </div>
   );
 }
